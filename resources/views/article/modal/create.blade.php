f68d2a1b21d2f7a2af6ec056e594a301a881df04
Shrmanto <shrmantoch@gmail.com>
2023-10-19 23:10:55 +0700

    style: form.modal CRUD & navbar

--- resources/views/article/modal/create.blade.php
+++ resources/views/article/modal/create.blade.php
@@ -10,22 +10,22 @@
                 @csrf
                 <div class="modal-body">
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="article_name">Article Name</label>
                         <input type="text" name="article_name" id="article_name" class="form-control" required>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="image">Image</label>
                         <input type="file" name="image" id="image" class="form-control">
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="description">Description</label>
                         <textarea name="description" id="description" class="form-control" required></textarea>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group mb-3">
                         <label for="categories_id">Category</label>
                         <select name="categories_id" id="categories_id" class="form-control" required>
                             <option value="">Select Category</option>
--- resources/views/components/landing_home/navbar.blade.php
+++ resources/views/components/landing_home/navbar.blade.php
@@ -7,7 +7,7 @@
             {{-- <span class="navbar-toggler-icon"></span> --}}
             <i class="fa-solid fa-ellipsis"></i>
         </button>
-        <div class="collapse navbar-collapse d-xl-flex justify-content-xl-center" id="navbarNav">
+        <div class="collapse navbar-collapse d-xl-flex align-items-xl-center justify-content-xl-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="#">Home</a>
@@ -15,24 +15,44 @@
                 <li class="nav-item">
                     <a class="nav-link mx-xl-3" href="#">News</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Categories
+                    </a>
+                    <ul class="dropdown-menu">
+                        @foreach ($categories as $cg)
+                            <li><a class="dropdown-item" href="#">{{ $cg->name_categories }}</a></li>
+                        @endforeach
+                    </ul>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link mx-xl-3" href="#">Contact</a>
+                </li>
+                @auth
+                <li class="nav-item dropdown ms-xl-4 d-xl-block d-none">
+                    <a class="nav-link nav-icon-hover" href="#" id="drop2" data-bs-toggle="dropdown"aria-expanded="false">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
+                        <div class="message-body">
+                            <a href="#" class="d-flex align-items-center gap-2 dropdown-item">
+                                <i class="ti ti-user fs-6"></i>
+                                <p class="mb-0 fs-3">My Profile</p>
+                            </a>
+                            <a href="#" class="d-flex align-items-center gap-2 dropdown-item">
+                                <i class="ti ti-mail fs-6"></i>
+                                <p class="mb-0 fs-3">My Account</p>
+                            </a>
+                            <a href="#" class="d-flex align-items-center gap-2 dropdown-item">
+                                <i class="ti ti-list-check fs-6"></i>
+                                <p class="mb-0 fs-3">My Task</p>
+                            </a>
+                            <a href="{{route('logout')}}" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
+                        </div>
+                    </div>
+                </li>
+                @endauth
             </ul>
         </div>
-        <div class="link-media">
-            <div class="content">
-                <a href="">
-                    <i class="fa-brands fa-instagram fa-2xl"></i>
-                </a>
-                <a href="" class="mx-xl-1">
-                    <i class="fa-brands fa-youtube fa-2xl"></i>
-                </a>
-                <a href="">
-                    <i class="fa-regular fa-envelope fa-2xl"></i>
-                </a>
-            </div>
-        </div>
-        <a href="{{route('logout')}}" type="button" class="btn btn-primary">logout</a>
     </div>
 </nav>
